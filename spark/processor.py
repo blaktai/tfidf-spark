58279c76d456f39be6160f3a89b5a9f6a13f33b6
blaktai <55172595+blaktai@users.noreply.github.com>
2020-12-13 23:57:09 -0500

    UPDATE fix formmatting

--- spark/processor.py
+++ spark/processor.py
@@ -2,31 +2,31 @@ from pyspark import SparkContext
 
 class TFIDFProcessor:
 
-@staticmethod
-def compute_tf_idf(sc, file_path):
-    """
-    
-    """
-    rdd = sc.textFile(file_path)
-    frequency_by_document = rdd.flatMap(lambda line: [[word + "_" + line.split(' ')[0], 1] for word in line.split(' ')[1:] if word != ''])\
-    .reduceByKey(lambda x, y: x + y)\
-    .map(lambda x: [x[0].split("_")[1], [x[0].split("_")[0],  x[1]]])
-    term_frequency = rdd.map(lambda line: [line.split(' ')[0], len(line.split(' ')[1:])])\
-                    .join(frequency_by_document)\
-                    .map(lambda pair: [pair[1][1][0], [pair[0], pair[1][1][1], pair[1][0]]])
-    total_documents = rdd.count()
-    word_counts = rdd \
-    .flatMap(lambda line: [[term, 1] for term in line.split(' ')[1:] if term != ""]) \
-    .reduceByKey(add) \
-    .map(lambda x: [x[0], log(total_documents / x[1])])
-    tf_idf_sparse_matrix = term_frequency.join(word_counts)\
-    .map(transform_tf_idf)\
-    .collect()
-    all_terms  = rdd.flatMap(extract_terms).filter(filter_empty_strings).distinct()
-    all_doc_ids = rdd.map(lambda line: line.split(' ')[0])
-    terms_map = all_terms.zipWithIndex().collectAsMap()
-    doc_map = all_doc_ids.zipWithIndex().collectAsMap()
-    return make_matrix(tf_idf_sparse_matrix, terms_map, doc_map))    
+    @staticmethod
+    def compute_tf_idf(sc, file_path):
+        """
+        
+        """
+        rdd = sc.textFile(file_path)
+        frequency_by_document = rdd.flatMap(lambda line: [[word + "_" + line.split(' ')[0], 1] for word in line.split(' ')[1:] if word != ''])\
+        .reduceByKey(lambda x, y: x + y)\
+        .map(lambda x: [x[0].split("_")[1], [x[0].split("_")[0],  x[1]]])
+        term_frequency = rdd.map(lambda line: [line.split(' ')[0], len(line.split(' ')[1:])])\
+                        .join(frequency_by_document)\
+                        .map(lambda pair: [pair[1][1][0], [pair[0], pair[1][1][1], pair[1][0]]])
+        total_documents = rdd.count()
+        word_counts = rdd \
+        .flatMap(lambda line: [[term, 1] for term in line.split(' ')[1:] if term != ""]) \
+        .reduceByKey(add) \
+        .map(lambda x: [x[0], log(total_documents / x[1])])
+        tf_idf_sparse_matrix = term_frequency.join(word_counts)\
+        .map(transform_tf_idf)\
+        .collect()
+        all_terms  = rdd.flatMap(extract_terms).filter(filter_empty_strings).distinct()
+        all_doc_ids = rdd.map(lambda line: line.split(' ')[0])
+        terms_map = all_terms.zipWithIndex().collectAsMap()
+        doc_map = all_doc_ids.zipWithIndex().collectAsMap()
+        return make_matrix(tf_idf_sparse_matrix, terms_map, doc_map))    
 
 
 def extract_terms(line):
@@ -60,3 +60,5 @@ def make_matrix(data, term_map, document_map):
             term_index = term_map[term]
             matrix[doc_index][term_index] = value
     return matrix
+
+class Consine
